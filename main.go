c2aa5e6d1cfedc52f85d6fdb36ee0cbb4d1481ba
TusharMohapatra07 <tusharmohapatra.gig@gmail.com>
2024-10-04 18:29:33 +0530

    create a hello world server

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+const port string = ":4000"
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello world"))
+}
 
 func main() {
-	fmt.Println("hello, world!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	log.Printf("Server started on port%s", port)
+	err := http.ListenAndServe(port, mux)
+	log.Fatal(err)
+
 }
